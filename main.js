649e876a21409f6eb0d943754e7bc14dd9bef20e
Sohee-Jeun <73260460+Sohee-Jeun@users.noreply.github.com>
2021-10-04 22:46:05 -0700

    basic fuction
    I should add some of functions.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+'use strict';
 const currentTime = document.querySelector('.date');
 const gameBtn = document.querySelector('.startButton');
 const popUp = document.querySelector('.pop-up');
 const popUpReplay = document.querySelector('.replayButton');
 const popUpExit = document.querySelector('.exitButton');
+const field = document.querySelector('.field');
+const icon = gameBtn.querySelector('.fas');
+const img = document.createElement('img');
 
 let started = false;
-let drink = undefined;
+let showRandomImg;
 
 gameBtn.addEventListener('click', ()=> {
     if(started){
@@ -14,53 +18,64 @@ gameBtn.addEventListener('click', ()=> {
         startGame();
     }
 });
+
 popUpReplay.addEventListener('click',()=>{
     startGame();
     hidePopUpBtns();
-})
+});
+
 popUpExit.addEventListener('click',()=>{
-    // finishGame();
+   
     console.log('game over');
-})
+    hidePopUpBtns();
+    init();
+});
 
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-
-today = mm + ' / ' + dd + ' / ' + yyyy;
-currentTime.innerText = today;
 
 function startGame(){
     started = true;
-    showStopButton();
-   // showRandomDrink();
-   console.log('start game');
-    
+    showStopBtn();
+    initGame();
+    showRandomImg = setInterval(()=>{
+       getRandom();
+    },100);
+   
 }
 
 function stopGame(){
     started = false;
+    
     showPopUpBtns();
-    hideStopButton();
+    hideStopBtn();
+    stopImg();
 }
 
-// function finishGame(){
-//     started = false;
-//     showPup
-// }
+function createImg(number){
+img.setAttribute('src', `imgs/${number}.png`);
+img.setAttribute('class','drinks_name');
+field.append(img);
 
-function showStopButton(){
-    const icon = gameBtn.querySelector('.fas');
+}
+function getRandom(){
+    const IMG_NUM = 17;
+    let num = Math.floor(Math.random() * IMG_NUM)+1;
+    createImg(num);
+
+}
+
+function showStopBtn(){
     icon.classList.add('fa-stop');
-    icon.classList.remove('.fa-play');
+    icon.classList.remove('fa-play');
     gameBtn.style.visibility= 'visible';    
 }
-// function showStartBtn(){
-//     gameBtn.style.visibility= 'visible';  
-// }
+ function showStartBtn(){
+     icon.classList.remove('fa-stop');
+     icon.classList.add('fa-play');
+    gameBtn.style.visibility= 'visible'; 
+}
 
-function hideStopButton(){
+//나중에 showStopButton과 showStartBtn을 if문으로 표현 할 수 있을 것 같애
+function hideStopBtn(){
     gameBtn.style.visibility ='hidden';
 }
 
@@ -75,6 +90,23 @@ function goBackFirst(){
     hidePopUpBtns();
     
 }
-function initLottery(){
+function stopImg(){
+    clearInterval(showRandomImg);
+}
 
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
+today = mm + ' / ' + dd + ' / ' + yyyy;
+currentTime.innerText = today;
+
+function init(){
+    field.removeChild(img);
+    showStartBtn();
+}
+
+function initGame(){
+    clearInterval(showRandomImg);
 }
